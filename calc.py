1d2b15b777f38c93f843d0c901277079192e03f0
allem <allemang.d@gmail.com>
2019-02-01 18:26:20 -0500

    first implementation of sub

--- calc.py
+++ calc.py
@@ -2,4 +2,14 @@ print('enter two numbers')
 a = int(input())
 b = int(input())
 
-print('the sum is', a + b)
+print('enter an operation')
+op = input()
+
+if op not in '+-':
+    print('unknown op')
+
+if op == '+':
+    print('the sum is', a + b)
+
+if op == '-':
+    print('the difference is', a - b)
