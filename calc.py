dcd4fb6ed3ab835ffbf3bbc73b18a1f1f448e8b7
Carlos <carlos.lopezrosario@yahoo.com>
2019-02-01 18:32:57 -0500

    Update calc.py

--- calc.py
+++ calc.py
@@ -5,7 +5,7 @@ b = int(input())
 print('enter an operation')
 op = input()
 
-if op not in '+-':
+if op not in '+-/':
     print('unknown op')
 
 if op == '+':
@@ -13,3 +13,6 @@ if op == '+':
 
 if op == '-':
     print('the difference is', a - b)
+
+if op == '/':
+    print('the quotient is', a / b)
